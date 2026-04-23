3562f7d9da317de8937c3c486e869a89415e40a4
Alex Silva <alexhctp@gmail.com>
2026-04-23 13:25:51 -0300

    Add newline to response message on root route

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let visitCount = 0;
 
 app.get('/', (req, res) => {
   visitCount++;
-  res.send(`Hello World - CI/CD funcionando! Até agora a pagina foi recarregada ${visitCount} vezes e contando.`);
+  res.send(`Hello World - CI/CD funcionando! Até agora a pagina foi recarregada ${visitCount} vezes e contando. \n`);
 });
 
 app.get('/status', (req, res) => {
